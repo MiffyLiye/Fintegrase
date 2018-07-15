f57b464cb3f0c3cc6e87dd305390aa14df25a19d
Wang, Tao <miffyliye@gmail.com>
2018-07-15 19:10:06 +0800

    better error message

--- app/middlewares/authHandler.js
+++ app/middlewares/authHandler.js
@@ -5,13 +5,13 @@ function authHandler() {
     // Respond 401 for unauthorized request
     if (ctx.request.headers['authorization'] !== process.env.AUTH_KEY &&
       ctx.request.headers['authorization'] !== process.env.ADMIN_AUTH_KEY) {
-      ctx.res.unauthorized(401, 'Unauthorized', {});
+      ctx.res.unauthorized('consumer_unauthorized', 'Consumer Authorization key is required', {});
       return;
     }
     if (ctx.request.method !== 'HEAD' &&
       ctx.request.method !== 'GET' &&
       ctx.request.headers['authorization'] !== process.env.ADMIN_AUTH_KEY) {
-      ctx.res.unauthorized(401, 'Unauthorized', {});
+      ctx.res.unauthorized('producer_unauthorized', 'Producer Authorization key is required', {});
       return;
     }
     return await next();
